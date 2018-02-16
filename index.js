610cc67b00b87b2eea3d346731582bcf59891ead
Dhivya <rollindiv@gmail.com>
2018-02-16 16:06:19 +0100

    update database

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var LocalStrategy = require('passport-local');
 var uid2 = require('uid2');
+var multer = require("multer");
+var upload = multer({
+  dest: "public/uploads/"
+});
 
 var SHA256 = require("crypto-js/sha256");
 
@@ -20,7 +24,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-
 var userSchema = new mongoose.Schema({
   "account": {
     "username": String,
@@ -29,18 +32,44 @@ var userSchema = new mongoose.Schema({
   "email": String,
   "token": String,
   "hash": String,
-  "salt": String
+  "salt": String,
+  "rooms": [{
+    "type": mongoose.Schema.Types.ObjectId,
+    "ref": "Room"
+  }]
+});
+
+var roomSchema = new mongoose.Schema({
+  "title": String,
+  "description": String,
+  "photos": [String],
+  "price": Number,
+  "ratingValue": Number,
+  "reviews": Number,
+  "city": String,
+  "loc": {
+    "type": [Number], // Longitude et latitude
+    "index": "2d" // Créer un index geospatial https://docs.mongodb.com/manual/core/2d/
+  },
+  "user": {
+    "type": mongoose.Schema.Types.ObjectId,
+    "ref": "User"
+  }
 });
 
 var Users = mongoose.model("Users", userSchema);
+var Rooms = mongoose.model("Rooms", roomSchema);
 
 app.post('/api/user/sign_up', function (req, res) {
-  var salt = uid2(10);
+
+  console.log("hello");
   var username = req.body.username;
+  var biography = req.body.biography;
   var email = req.body.email;
   var password = req.body.password;
-  var biography = req.body.biography;
+  var salt = uid2(10);
   var hash = SHA256(req.body.password + salt).toString(encBase64);
+  var photos = req.body.photos;
 
   var newUser = new Users({
     email: email,
@@ -52,14 +81,66 @@ app.post('/api/user/sign_up', function (req, res) {
     }
   });
 
-  newUser.save(function (err, obj) {
+  newUser.save(function (err, userObj) {
     if (!err) {
       console.log("we just saved the new user ");
+      return res.json(userObj);
+    }
+    console.log("something went wrong");
+  });
+});
+
+function getUser(req, res, next) {
+  var auth = req.headers.authorization;
+  var token = auth.split(" ")[1];
+  Users.findOne({
+    token: token
+  }, function (err, user) {
+    if (user) {
+      req.user_id = user._id;
+      req.username = user.account.username;
+      next();
+    } else {
+      res.json({
+        message: "No user found"
+      })
+    }
+  });
+}
+
+
+app.post('/api/room/publish', getUser, function (req, res) {
+  console.log("user_id", req.user_id)
+
+  var title = req.body.title;
+  var description = req.body.description;
+  var ratingValue = req.body.ratingValue;
+  var reviews = req.body.reviews;
+  var photos = req.body.photos;
+  var price = req.body.price;
+  var city = req.body.city;
+  var loc = req.body.loc;
+
+  var newRoom = new Rooms({
+    title: title,
+    description: description,
+    photos: photos,
+    price: price,
+    reviews: reviews,
+    ratingValue: ratingValue,
+    city: city,
+    loc: loc,
+    user: req.user_id
+  });
+  newRoom.save(function (err, obj) {
+    if (!err) {
+      console.log("we just saved the new room ");
       return res.json(obj);
     }
     console.log("something went wrong");
   });
 });
+
 
 app.post('/api/user/log_in', function (req, res) {
 
@@ -69,14 +150,14 @@ app.post('/api/user/log_in', function (req, res) {
   Users.findOne({
     email: user_email
   }).exec(function (err, user) {
-    console.log(user.token);
-
     if ((SHA256(user_password + user.salt).toString(encBase64)) == user.token) {
       return res.json(user);
     } else res.send("not logged");
   });
 
 });
+
+
 
 app.get("/api/user/:id", function (req, res) {
   var id = req.params.id;
@@ -86,8 +167,8 @@ app.get("/api/user/:id", function (req, res) {
   Users.findOne({
     _id: id
   }).exec(function (err, user) {
-    console.log(user.token); 
-    if(user_token == ('Bearer '+ user.token)){
+    console.log(user.token);
+    if (user_token == ('Bearer ' + user.token)) {
       return res.json(user);
     } else return res.send("Unauthorized");
   });
